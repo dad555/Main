4243927d93f2a43d95cbf274279b222b62b5f6d3
Artur <artur@briginvest.ru>
2018-09-27 14:56:43 +0300

    commit

---
+++ Eckel/src/control/ListCharacters.java
@@ -0,0 +1,12 @@
+package control;
+
+// Перебор вмех ASCII-символов нижнего регистра
+
+public class ListCharacters {
+    public static void main(String[] args) {
+        for (char c = 0; c < 128; c++) {
+            if(Character.isLowerCase(c))
+            System.out.println("значение: " + (int) c + " символ: " + c);
+        }
+    }
+}
--- Eckel/src/operators/PassObject.java
+++ Eckel/src/operators/PassObject.java
@@ -1,3 +1,5 @@
+package operators;
+
 class Letter {
     char c;
 }
