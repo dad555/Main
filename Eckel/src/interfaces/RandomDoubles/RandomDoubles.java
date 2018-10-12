e7d53d4d5c63cff2c0f39b27e6aee181efafa940
Artur <artur@briginvest.ru>
2018-10-12 17:28:29 +0300

    commit

---
+++ Eckel/src/interfaces/RandomDoubles/RandomDoubles.java
@@ -0,0 +1,17 @@
+package interfaces.RandomDoubles;
+
+import java.util.*;
+
+public class RandomDoubles {
+    private static Random r = new Random(47);
+    public double next() {
+        return r.nextDouble();
+    }
+
+    public static void main(String[] args) {
+        RandomDoubles rd = new RandomDoubles();
+        for(int i = 0; i < 7; i++) {
+            System.out.print(rd.next() + " ");
+        }
+    }
+}
