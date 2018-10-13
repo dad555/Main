311c4ebfde063dbdb85af895bf30f5a06cd8a457
dad555 <Art101985>
2018-10-13 19:32:25 +0300

    Initial commit

---
+++ Learning Java/src/gui/SimpleGui1B.java
@@ -0,0 +1,30 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGui1B implements ActionListener {
+    JButton button;
+
+    public static void main(String[] args) {
+        SimpleGui1B gui = new SimpleGui1B();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("нажми меня");
+
+        button.addActionListener(this);
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        button.setText("меня нажали");
+    }
+}
