48f265b20983cd3f816e1429430b22764e0b96d0
Artur <artur@briginvest.ru>
2018-10-16 17:31:34 +0300

    Merge branch 'master' of https://github.com/dad555/Main
    # Conflicts:
    #	.idea/vcs.xml

--- src/test1.java
+++ src/test1.java
@@ -1,20 +1,18 @@
+import java.io.File;
+
 public class test1 {
     public static void main(String[] args) {
-        while(condition()) {
-            System.out.println("Inside 'while'");
+        File f = new File("MyTestFile.txt");
+        File dir = new File("Chapter7");
+        dir.mkdir();
+
+        if(dir.isDirectory()) {
+            for(String s : dir.list()) {
+                System.out.println(s);
+            }
         }
-        System.out.println("Exited 'while'");
-        System.out.println(Math.random());
 
-
-        for (char c = 0; c < 128; c++) {
-            if (Character.isLowerCase(c)) System.out.println("значение: " + (int) c + " символ: " + c);
-        }
-    }
-
-    static boolean condition() {
-        boolean result = Math.random() < 0.09;
-        System.out.println(result + ", ");
-        return result;
+        System.out.println(dir.getAbsoluteFile());
+        System.out.println(f.delete());
     }
 }
