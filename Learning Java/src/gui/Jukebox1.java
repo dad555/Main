23e04a941ec6e666189690062098d401607b3963
Artur <artur@briginvest.ru>
2018-10-22 17:49:31 +0300

    Merge branch 'master' of https://github.com/dad555/Main
    # Conflicts:
    #	.idea/vcs.xml

---
+++ Learning Java/src/gui/Jukebox1.java
@@ -0,0 +1,109 @@
+package gui;
+
+import java.io.*;
+import java.util.*;
+
+public class Jukebox1 {
+    ArrayList<Song> songList = new ArrayList<Song>();
+
+    public static void main(String[] args) {
+        new Jukebox1().go();
+    }
+
+    class ArtistCompare implements Comparator<Song> {
+        @Override
+        public int compare(Song o1, Song o2) {
+            return o1.getArtist().compareTo(o2.getArtist());
+        }
+    }
+
+    public void go() {
+        getSongs();
+        System.out.println(songList);
+        Collections.sort(songList);
+        System.out.println(songList);
+
+        ArtistCompare artistCompare = new ArtistCompare();
+        Collections.sort(songList, artistCompare);
+
+        System.out.println(songList);
+
+        System.out.println();
+
+        HashSet<Song> songSet = new HashSet<>();
+        songSet.addAll(songList);
+        System.out.println(songSet);
+    }
+
+    void getSongs() {
+        try {
+            File file = new File("SongList.txt");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                addSong(line);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    void addSong(String lineToParse) {
+        String[] tokens = lineToParse.split("/");
+        Song nextSong = new Song(tokens[0], tokens[1], tokens[2], tokens[3]);
+        songList.add(nextSong);
+    }
+}
+
+class Song implements Comparable<Song> {
+    String title;
+    String artist;
+    String rating;
+    String bpm;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return getTitle().equals(song.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return title.hashCode();
+    }
+
+    Song(String t, String a, String r, String b) {
+        title = t;
+        artist = a;
+        rating = r;
+        bpm = b;
+    }
+
+    @Override
+    public int compareTo(Song o) {
+        return title.compareTo(o.getTitle());
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public String getBpm() {
+        return bpm;
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
---
+++ src/findCyrillicCount.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class findCyrillicCount {
+
+    public static void main(String[] args) {
+        new findCyrillicCount().go();
+    }
+
+    public void go() {
+        try {
+            File file = new File("C:\\Users\\artur\\Downloads\\accounts-operations.xsd");
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String line = null;
+            int row = 0;
+            int count = 0;
+            ArrayList<Character> c = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                row++;
+                for (int i = 0; i < line.length(); i++) {
+                    if (Character.UnicodeBlock.of(line.charAt(i)).equals(Character.UnicodeBlock.CYRILLIC)) {
+                        count++;
+                        c.add(line.charAt(i));
+                    }
+                }
+
+                System.out.println("Линия: " + row + " Кол-во: " + count + " " + c.toString());
+                count = 0;
+                c.clear();
+            }
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+}
