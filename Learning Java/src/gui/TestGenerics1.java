1a03ac06696c941b3ebff4b017f1f0004f2248dd
Artur <artur@briginvest.ru>
2018-10-25 12:21:00 +0300

    Merge branch 'master' of https://github.com/dad555/Main
    # Conflicts:
    #	.idea/vcs.xml

---
+++ Learning Java/src/gui/TestGenerics1.java
@@ -0,0 +1,42 @@
+package gui;
+
+import java.util.ArrayList;
+
+public class TestGenerics1 {
+    public static void main(String[] args) {
+        new TestGenerics1().go();
+    }
+
+    public void go() {
+        ArrayList<Animal> animals = new ArrayList<>();
+        animals.add(new Dog());
+        animals.add(new Cat());
+        animals.add(new Dog());
+        takeAnimals(animals);
+
+        ArrayList<Dog> dogs = new ArrayList<>();
+        dogs.add(new Dog());
+        dogs.add(new Dog());
+        takeAnimals(dogs);
+    }
+
+    public void takeAnimals (ArrayList<? extends Animal> animals) {
+        for (Animal a : animals) a.eat();
+    }
+}
+
+abstract class Animal {
+    void eat() {
+        System.out.println("Животное ест");
+    }
+}
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Гав");
+    }
+}
+
+class Cat extends Animal {
+    void meow() { }
+}
