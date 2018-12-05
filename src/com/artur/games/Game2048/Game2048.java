4962e0b3843d913aa296efe217d52c72aa9ba591
Artur <artur@briginvest.ru>
2018-12-05 10:51:09 +0300

    Merge branch 'master' of https://github.com/dad555/Main
    # Conflicts:
    #	.idea/vcs.xml

---
+++ src/com/artur/games/Game2048/Game2048.java
@@ -0,0 +1,13 @@
+package com.artur.games.Game2048;
+
+import javax.swing.*;
+
+public class Game2048 extends JPanel {
+    private static final int SIDE = 4;
+    private int[][] gameField = new int[SIDE][SIDE];
+    private JFrame mainFrame = new JFrame();
+
+    public void initialize() {
+
+    }
+}
