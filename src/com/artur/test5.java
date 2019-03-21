4346778dcce5c7e1929568d6893f94ef3a75b97e
Artur <artur@briginvest.ru>
2019-03-21 15:26:07 +0300

    Merge branch 'master' of https://github.com/dad555/Main
    # Conflicts:
    #	.idea/vcs.xml

---
+++ src/com/artur/test5.java
@@ -0,0 +1,19 @@
+package com.artur;
+
+public class test5 {
+    public static void main(String[] args) {
+        int i = 5;
+
+        while (i < 5) {
+            i++;
+        }
+
+        System.out.println(i);
+
+        do {
+            i++;
+        } while (i < 5);
+
+        System.out.println(i);
+    }
+}
