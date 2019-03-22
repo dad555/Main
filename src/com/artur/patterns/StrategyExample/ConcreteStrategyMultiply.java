51636af9f742e58bb30b860f57153d7a96649aef
Artur <artur@briginvest.ru>
2019-03-22 17:01:13 +0300

    Merge branch 'master' of https://github.com/dad555/Main
    # Conflicts:
    #	.idea/vcs.xml

--- src/com/artur/patterns/StrategyExample/ConcreteStrategyMultiply.java
+++ src/com/artur/patterns/StrategyExample/ConcreteStrategyMultiply.java
@@ -2,6 +2,7 @@ package com.artur.patterns.StrategyExample;
 
 // Реализуем алгоритм с использованием интерфейса стратегии
 public class ConcreteStrategyMultiply implements Strategy {
+    @Override
     public int execute(int a, int b) {
         System.out.println("Called ConcreteStrategyMultiply's execute()");
         return a * b;   // Do a multiplication with a and b
