eb0e0f8793c02c86ebc6fcbed37c86a2d89f4183
Artur <artur@briginvest.ru>
2019-05-07 10:51:55 +0300

    Merge branch 'master' of https://github.com/dad555/Main
    # Conflicts:
    #	.idea/vcs.xml

---
+++ src/com/artur/games/Dva/Move.java
@@ -0,0 +1,6 @@
+package com.artur.games.Dva;
+
+@FunctionalInterface
+public interface Move {
+    void move();
+}
--- src/com/artur/test5.java
+++ src/com/artur/test5.java
@@ -1,32 +1,11 @@
 package com.artur;
 
-import sun.misc.VM;
+import java.util.*;
 
 public class test5 {
     public static void main(String[] args) {
-        int x = 100;
-        Integer y = new Integer(100);
-        Integer z = 100;
-
-        System.out.println(x==y); // true
-        System.out.println(x==z); // true
-        System.out.println(y==z); // true
-        System.out.println(y.getClass().getSimpleName().hashCode());
-        System.out.println(z.getClass().hashCode());
-
-
-
-        System.out.println();
-
-        int _x = 300;
-        Integer _y = 300;
-        Integer _z = 300;
-
-        System.out.println(_x==_y); // true
-        System.out.println(_x==_z); // true
-        System.out.println(_y==_z); // true
-
-        System.out.println(_y.getClass().getSimpleName().hashCode());
-        System.out.println(_z.getClass().getSimpleName().hashCode());
+        double x = -1.0 / 0.0;
+        double y = 1.0 / 0.0;
+        System.out.println(y * -1);
     }
 }
